54fb6207c01bd5602cc6289b3e20714bacc28bd2
sumitshinde-84 <shindesumit217@gmail.com>
2022-10-10 15:18:44 +0530

    new ui added

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ function genRandNo() {
 
     let randNo = Math.floor(Math.random() * (3 - 1)) + 1;
     return randNo;
-
 }
+
+
+let userPoint=0;
+let computerPoint=0;
+let round =0;
+let userChoice;
+let computerChoice;
+let player = document.querySelector(".player")
+let com = document.querySelector(".com")
+let final= document.querySelector("#final");
+let paraCom= document.querySelector('#comC')
 
 
 // get computer choice 
@@ -15,16 +25,22 @@ function computerChoicefun() {
     let randNo = genRandNo();
 
     if (randNo == 1){
+       
+
+            
+            paraCom.innerHTML="Computer choose : 'Rock'"
+    
         return 'Rock';
     }
 
-    else if (randNo == 2){
+    else if (randNo ==3){
+        paraCom.innerHTML="Computer choose : 'Paper'"
         return 'Paper';
     }
 
 
-    else if (randNo == 3)
-{ 
+    else if (randNo ==2)
+{     paraCom.innerHTML="Computer choose : 'Scissor'"
        return 'Scissor';
 }
 
@@ -35,111 +51,352 @@ function computerChoicefun() {
 }
 
 
-// Get user choice
-
-function getUserChoice() {
 
 
-    let input = prompt("enter your choice..?");
-    let ans = input.toLowerCase();
-    ans = ans.charAt(0).toUpperCase() + ans.slice(1);
-    return ans;
 
-}
+// click event for options 
+
+let rock = document.querySelector('.col1')
+ rock.addEventListener('click', getUserChoiceRock)
+ rock.addEventListener('click',game1);
+ rock.addEventListener('click',()=>{
+
+    let para= document.querySelector('#userC')
+    para.innerHTML="your choice :'Rock'"
+})
 
 
-// Check userinput 
 
-function checkUserChoice(userChoice){
+
+
+// paper event
+
+let paper = document.querySelector('.col2')
+paper.addEventListener('click', getUserChoicePaper)
+paper.addEventListener('click',game2);
+paper.addEventListener('click',()=>{
+
+    // message 
+
+    let para= document.querySelector('#userC')
+    para.innerHTML="your choice :'Paper'"
+})
+
+// scissor event 
+
+let scissor = document.querySelector('.col3')
+scissor.addEventListener('click', getUserChoiceScissor)
+scissor.addEventListener('click',game3);
+scissor.addEventListener('click',()=>{
+
+// message
+
+    let para= document.querySelector('#userC')
+    para.innerHTML="your choice :'Scissor'"
+})
     
 
-    switch(userChoice){
-        case "Rock": 
-            return true;
-           
-        case "Paper": 
-            return true;
-            
-        case "Scissor":
-            return true;
-        
 
-        default:
-            alert("Input is wrong!");
-            return false;
-    }
+
+
+
+
+
+
+
+// Get user choice for rock
+
+function getUserChoiceRock() {
+
+
+   let userChoice = 'Rock';
+   
+    return userChoice;
+    
+
 }
-// playround
+
+
+
+
+
+// Get user choice for paper
+
+function getUserChoicePaper() {
+
+    let userChoice = 'Paper';
+    
+    return userChoice;
+   
+    
+ 
+ }
+
+
+ // Get user choice for scissor
+
+ function getUserChoiceScissor() {
+
+    let userChoice = 'Scissor';
+    
+   
+    
+    return userChoice;
+   
+    
+ 
+ }
+
+
+
+
+// playround function
 
 function playRound(userChoice, computerChoice) {
+   
+    let user = document.querySelector('#userpointer')
+    let computer =document.querySelector("#computerPointer");
 
-    if (userChoice == 'Rock' && computerChoice == 'Scissor')
-        return true;
+// display winner after final round
 
-    else if (userChoice == 'Paper' && computerChoice == 'Rock')
-        return true;
+    if(round==5){
+        displayWinner(userPoint, computerPoint);
+        
+    }
 
-    else if (userChoice == 'Scissor' && computerChoice == 'Paper')
-        return true;
+// restart game
 
-    else if (userChoice == computerChoice)
-        return "tie";
+
+    if(round>5  ){
+        round=0;
+        userPoint=0;
+        computerPoint=0;
+        
+        user.innerHTML=`${userPoint}`;
+        computer.innerHTML=`${computerPoint}`
+        player.style=''
+    com.style=' '
     
-        return false;
+   
+    
+    }
+
+    
+    // round play starts from 
+
+    if (userChoice == 'Rock' && computerChoice == 'Scissor'){
+        userPoint++;
+        user.innerHTML=`${userPoint}`;
+        computer.innerHTML=`${computerPoint}`
+        round++;    
+    return true;
+
+}
+        
+    
+    else if (userChoice == 'Paper' && computerChoice == 'Rock'){
+       
+        userPoint++;
+    user.innerHTML=`${userPoint}`
+    computer.innerHTML=`${computerPoint}` 
+    round++  
+    return true;
+    }
+
+    else if (userChoice == 'Scissor' && computerChoice == 'Paper'){
+       
+        userPoint++;
+    user.innerHTML=`${userPoint}`
+    computer.innerHTML=`${computerPoint}`
+    round++
+        
+    return true;
+    }
+
+    else if (userChoice == computerChoice){
+    
+        round++;    
+    return "tie";}
+
+
+     else{ 
+      computerPoint++;
+      computer.innerText=`${computerPoint}`
+     
+      round++;
+    }
+    
+      return false;
+
+      
 }
 
 
-function game() {
-
-
-    let round = 0;
-    let userPoint = 0;
-    let computerPoint = 0;
-
-    while (round < 5) {
 
 
 
 
 
-        let userChoice = getUserChoice();
+
+
+
+
+
+//    function for rock click 
+
+function game1( ) {
+
+
+    
+
+    
+
+ 
+
+    let userChoice = getUserChoiceRock();
         let computerChoice = computerChoicefun();
+    
 
 
-        let check = checkUserChoice(userChoice);
 
-        if (check) {
+    // audio on click
+
+    const audio = new Audio("audio/clicked.wav");
+        audio.play();
+     
+    
+             
+             
             let result = playRound(userChoice, computerChoice);
+            let beat = document.querySelector('#beat') 
+            let head = document.querySelector('#head');
+
 
             if (result == true) {
-                alert(`you win ${userChoice} beats ${computerChoice}`)
-                userPoint++;
+                beat.innerHTML= `${userChoice} beats ${computerChoice}`
+                head.innerHTML= `You Win This Round`
+               
             }
 
             else if (result == false) {
-                alert(`you loose ${computerChoice} beats ${userChoice}`)
-                computerPoint++;
+                beat.innerHTML= `${computerChoice} beats ${userChoice}`
+                head.innerHTML= `You Loose This Round`
+               
 
             }
             else if(result=="tie") {
-                alert("It's a Tie...!");
+             beat.innerHTML= `It's Tie`
+             head.innerHTML= `yeahh It's Tie`
             }
-            round++;
-
-        }
-        else {
-            alert("please choose correct choice")
-        }
-
-
-        alert(`Total Round = ${round}`);
-        alert(`Your points = ${userPoint} & Computer points = ${computerPoint}`);
-    }
-
-    displayWinner(userPoint, computerPoint);
+          
+     
+        
+     
 
 }
 
+
+// function for paper click
+
+function game2( ) {
+
+
+    
+
+    
+
+
+
+    let  userChoice = getUserChoicePaper();
+    let computerChoice = computerChoicefun();
+
+
+     // audio on click
+
+     const audio = new Audio("audio/clicked.wav");
+     audio.play();
+
+  
+    let result = playRound(userChoice, computerChoice);
+    let beat = document.querySelector('#beat') 
+    let head = document.querySelector('#head');
+
+
+    if (result == true) {
+        beat.innerHTML= `${userChoice} beats ${computerChoice}`
+        head.innerHTML= `You Win This Round`
+        
+    }
+
+    else if (result == false) {
+        beat.innerHTML= `${computerChoice} beats ${userChoice}`
+        head.innerHTML= `You Loose This Round`
+        
+
+    }
+    else if(result=="tie") {
+     beat.innerHTML= `It's Tie`
+     head.innerHTML= `yeahh It's Tie`
+     
+    }
+    
+
+
+     
+        
+  
+
+}
+
+// function for scissor click
+
+function game3( ) {
+
+
+    
+
+    
+
+
+   
+    let userChoice = getUserChoiceScissor();
+    let computerChoice = computerChoicefun();
+
+
+    let result = playRound(userChoice, computerChoice);
+    let beat = document.querySelector('#beat') 
+    let head = document.querySelector('#head');
+
+
+ // audio on click
+
+ const audio = new Audio("audio/clicked.wav");
+ audio.play();
+     
+
+    if (result == true) {
+        beat.innerHTML= `${userChoice} beats ${computerChoice}`
+        head.innerHTML= `You Win This Round`
+        
+    }
+
+    else if (result == false) {
+        beat.innerHTML= `${computerChoice} beats ${userChoice}`
+        head.innerHTML= `You Loose This Round`
+        
+
+    }
+    else if(result=="tie") {
+     beat.innerHTML= `It's Tie`
+     head.innerHTML= `yeahh It's Tie`
+    }
+  
+    
+    
+}
+
+
+// function for decide final winner 
 
 
 function displayWinner(userPoint, computerPoint) {
@@ -147,19 +404,37 @@ function displayWinner(userPoint, computerPoint) {
 
     if (userPoint > computerPoint) {
 
-        alert('you are final winnner now...');
-        document.write('<h2>you are final winnner now...</h2>')
-    }
-
+        let player = document.querySelector(".player")
+        player.style='box-shadow:10px 5px gold;'
+        const audio2 = new Audio("audio/win.mp3");
+         audio2.play();
+         
+         final.innerText='You Win Congratulation..'
+         final.style='font-size:30px'
+         
+        
+    } 
     else if (userPoint < computerPoint) {
-        alert('you loose ....sorry!');
-        document.write('<h2>loooserrr......</h2>')
+       
+        com.style='box-shadow:10px 5px gold; '
+        const audio3 = new Audio("audio/lose.wav");
+         audio3.play();
+         final.innerText='You loose.....'
+         final.style='font-size:30px'
+      
+   
     }
 
     else {
 
-        alert('game is Tie......')
-        document.write('<h2>It\'s Tie</h2>')
+        const audio4 = new Audio("audio/tie.wav");
+         audio4.play();
+        final.innerText='It\'s Tie.....'
+         final.style='font-size:30px'
+        
     }
+
+    
 }
+
 
